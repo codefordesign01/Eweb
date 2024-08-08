2de8925cd035525e1d710756dc268809a312c15a
Shahzad Ahmad <shahzadmughals278@gmail.com>
2024-08-08 17:12:25 +0500

    add navbar and router

--- my-app/src/component/footer/Footer.js
+++ my-app/src/component/footer/Footer.js
@@ -1,9 +1,82 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const Footer = () => {
   return (
     <div>
-      Footer
+      <footer>
+         <div className="container">
+            <div className="row">
+               <div className="col-md-4">
+                   <div className="full">
+                      <div className="logo_footer">
+                        <Link to="/"><img width="150" src="images/logo.png" alt="#" /></Link>
+                      </div>
+                      <div className="information_f">
+                        <p><strong>ADDRESS:</strong> 28 White tower, Street Name New York City, USA</p>
+                        <p><strong>TELEPHONE:</strong> +91 987 654 3210</p>
+                        <p><strong>EMAIL:</strong> yourmain@gmail.com</p>
+                      </div>
+                   </div>
+               </div>
+               <div className="col-md-8">
+                  <div className="row">
+                  <div className="col-md-7">
+                     <div className="row">
+                        <div className="col-md-6">
+                     <div className="widget_menu">
+                        <h3>Menu</h3>
+                        <ul>
+                           <li><Link to="/">Home</Link></li>
+                           <li><Link to="/">About</Link></li>
+                           <li><Link to="/">Services</Link></li>
+                           <li><Link to="/">Testimonial</Link></li>
+                           <li><Link to="/">Blog</Link></li>
+                           <li><Link to="/">Contact</Link></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div className="col-md-6">
+                     <div className="widget_menu">
+                        <h3>Account</h3>
+                        <ul>
+                           <li><Link to="/">Account</Link></li>
+                           <li><Link to="/">Checkout</Link></li>
+                           <li><Link to="/">Login</Link></li>
+                           <li><Link to="/">Register</Link></li>
+                           <li><Link to="/">Shopping</Link></li>
+                           <li><Link to="/">Widget</Link></li>
+                        </ul>
+                     </div>
+                  </div>
+                     </div>
+                  </div>     
+                  <div className="col-md-5">
+                     <div className="widget_menu">
+                        <h3>Newsletter</h3>
+                        <div className="information_f">
+                          <p>Subscribe by our newsletter and get update protidin.</p>
+                        </div>
+                        <div className="form_sub">
+                           <form>
+                              <fieldset>
+                                 <div className="field">
+                                    <input type="email" placeholder="Enter Your Mail" name="email" />
+                                    <input type="submit" value="Subscribe" />
+                                 </div>
+                              </fieldset>
+                           </form>
+                        </div>
+                     </div>
+                  </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <div className="cpy_">
+         <p>© 2021 All Rights Reserved By <Link href="https://html.design/">Free Html Templates</Link></p>
+      </div>
     </div>
   )
 }
--- my-app/src/component/navbar/Navbar.js
+++ my-app/src/component/navbar/Navbar.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div>
-      Navbar
+      <header className="header_section">
+            <div className="container">
+               <nav className="navbar navbar-expand-lg custom_nav-container ">
+                  <Link className="navbar-brand" to="/"><img width="100" src="/images/logo.png" alt="#" /></Link>
+                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className=""> </span>
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul className="navbar-nav">
+                        <li className="nav-item active">
+                           <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+                        </li>
+                        <li className="nav-item">
+                           <Link className="nav-link" to="/about">About Us</Link>
+                        </li>
+                        <li className="nav-item">
+                           <Link className="nav-link" to="/product">Products</Link>
+                        </li>
+                        <li className="nav-item">
+                           <Link className="nav-link" to="/contact">Contact</Link>
+                        </li>
+                        <li className="nav-item">
+                           <Link className="nav-link" to="cart">
+                             cart
+                           </Link>
+                        </li>
+                     </ul>
+                  </div>
+               </nav>
+            </div>
+         </header>
     </div>
   )
 }
